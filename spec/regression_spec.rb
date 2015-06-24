45a614e3a45e9524237e70bd535e95d8c7f9a8ab
Sam Han <shan@8thlight.com>
2015-06-24 12:41:40 +0100

    add tictactoe_core.rb file

--- spec/regression_spec.rb
+++ spec/regression_spec.rb
@@ -5,40 +5,41 @@ require 'tictactoe_core/game'
 require 'tictactoe_core/board'
 require 'tictactoe_core/board_helper'
 
-describe 'check full games end in draw with minimax', :slow => true do
-  let(:ui) { TicTacToe::Fakes::UiMock.new }
-  let(:ai) { TicTacToe::Ai::MinimaxAi.new }
-  let(:player_factory) { TicTacToe::Factory::PlayersFactory.new(ui, ai) }
-  let(:players) { player_factory.create(TicTacToe::Factory::PlayersFactory::CVC_GAME_TYPE) }
+module TicTacToeCore
+  describe 'check full games end in draw with minimax', :slow => true do
+    let(:ui) { Fakes::UiMock.new }
+    let(:ai) { Ai::MinimaxAi.new }
+    let(:player_factory) { Factory::PlayersFactory.new(ui, ai) }
+    let(:players) { player_factory.create(Factory::PlayersFactory::CVC_GAME_TYPE) }
 
-  it 'plays 5 games and all end in a draw' do
-    5.times do |i|
-      game = TicTacToe::Game.new(TicTacToe::Board.make_board(4), players, ui)
+    it 'plays 5 games and all end in a draw' do
+      5.times do |i|
+        game = Game.new(Board.make_board(4), players, ui)
+        game.run
+        expect(game.end_game_state).to eq(Board::DRAW)
+      end
+    end
+
+    it 'draws in a previously winning game setup' do
+      board = BoardHelper.create_initial_board_four('-OX-O-----X----X')
+      game = Game.new(board, players, ui)
+      game.update_current_player
       game.run
-      expect(game.end_game_state).to eq(TicTacToe::Board::DRAW)
+      expect(game.end_game_state).to eq(Board::DRAW)
+    end
+
+    it 'draws in a previously winning game setup 2' do
+      board = BoardHelper.create_initial_board_four('O--X----O--X--X-')
+      game = Game.new(board, players, ui)
+      game.update_current_player
+      game.run
+      expect(game.end_game_state).to eq(Board::DRAW)
+    end
+
+    it 'plays a C v C game to a draw' do
+      game = Game.new(Board.make_board(4), players, ui)
+      game.run
+      expect(game.end_game_state).to eq(Board::DRAW)
     end
   end
-
-  it 'draws in a previously winning game setup' do
-    board = TicTacToe::BoardHelper.create_initial_board_four('-OX-O-----X----X')
-    game = TicTacToe::Game.new(board, players, ui)
-    game.update_current_player
-    game.run
-    expect(game.end_game_state).to eq(TicTacToe::Board::DRAW)
-  end
-
-  it 'draws in a previously winning game setup 2' do
-    board = TicTacToe::BoardHelper.create_initial_board_four('O--X----O--X--X-')
-    game = TicTacToe::Game.new(board, players, ui)
-    game.update_current_player
-    game.run
-    expect(game.end_game_state).to eq(TicTacToe::Board::DRAW)
-  end
-
-  it 'plays a C v C game to a draw' do
-    game = TicTacToe::Game.new(TicTacToe::Board.make_board(4), players, ui)
-    game.run
-    expect(game.end_game_state).to eq(TicTacToe::Board::DRAW)
-  end
 end
-
